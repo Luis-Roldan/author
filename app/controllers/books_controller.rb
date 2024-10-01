f214c98bba2a96236a6ff6f869476023970a7d86
luis roldan <roldanluis022@gmail.com>
2024-10-01 18:15:53 -0500

    to fix new book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,16 +6,19 @@ class BooksController < ApplicationController
   def show
     @creator = Creator.find(params[:id])
     @books = Book.all
-    @books_by_creator = @books.select{|b| b.creator_id == @creator.id}
-
+    @books_by_creator = @books.select { |b| b.creator_id == @creator.id }
   end
 
   def new
     @book = Book.new
+    @creator = Creator.find(params[:creator_id])
   end
 
   def create
+    @creator = Creator.find(params[:creator_id])
+
     @book = Book.new(book_params)
+
     if @book.save
       redirect_to @book, notice: "book was created successfully"
     else
@@ -30,6 +33,7 @@ class BooksController < ApplicationController
   end
 
   private
+
 
   def book_params
     params.require(:book).permit(:title, :pages)
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
   belongs_to :creator
-  validates :title, uniqueness: true, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :pages, numericality: { only_integer: true }
 end
