bd6ee2507ae994be37d6aeed34cfc6e0dc562694
luis roldan <roldanluis022@gmail.com>
2024-10-02 18:02:55 -0500

    edit, new book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -64,6 +64,6 @@ class BooksController < ApplicationController
   end
 
   def book_params
-    params.require(:book).permit(:title, :pages)
+    params.require(:book).permit(:title, :pages, :publisher, :genre, :rating)
   end
 end
