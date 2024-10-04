c4eb08cbc74be924cc8e722916d0130b6a6a92a7
luis roldan <roldanluis022@gmail.com>
2024-10-03 21:30:31 -0500

    not fixed bug

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,3 +2,6 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+import { application } from "./application";
+import HomepageController from "./homepage_controller";
+application.register("homepage", HomepageController);
